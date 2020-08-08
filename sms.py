f554165fe6c0d93780ddac61254dabe38ada111b
Shubham Kshatriya <shubhamkshatriya521@gmail.com>
2020-08-08 18:34:37 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,32 +1,11 @@
 from twilio.rest import Client
 
-# # Your Account Sid and Auth Token from twilio.com/console
-# # DANGER! This is insecure. See http://twil.io/secure
-# account_sid = 'ACee6ec8d2c1aaa94e940daa9507a6a034'
-# auth_token = '5a96e76da74c17740ac78984f98aa144'
-# client = Client(account_sid, auth_token)
-
-# message = client.messages \
-#                 .create(
-#                      body="Join Earth's mightiest heroes.",
-#                      from_='+14752566797',
-#                      to='+917228801122'
-#                  )
-
-# print(message.sid)
-
 import schedule
 import sqlite3
 from datetime import timedelta
 import datetime
 from django.core.mail import send_mail
 from django.conf import settings
-# def sms():
-#     hear = Hearing.objects
-#     schedule.every(10).seconds.do(play)
-#     schedule.every().day.at("22:13").do(hey)
-    
-
 
 def job():
     db = sqlite3.connect('db.sqlite3')
@@ -56,21 +35,21 @@ def job():
             cur.execute("SELECT case_no, title FROM backend_hearing WHERE nextHearing='"+str(i)+"' and conclusion='"+str(j)+"'")
             r = cur.fetchall()
                         
-            account_sid = 'ACee6ec8d2c1aaa94e940daa9507a6a034'
-            auth_token = '5a96e76da74c17740ac78984f98aa144'
+            account_sid = 'ACee6ec8d2c1aaxxxxxxxxxxxxxxxxx'
+            auth_token = '5a96e76da74c177xxxxxxxxxxxxxxxxx'
             client = Client(account_sid, auth_token)
 
             subject = 'LegalWiz account password reset'
             message = "Your case number: "+str(r[0][0])+", and case title: "+str(r[0][1])+", is scheduled on: "+str(i)
             email_from = settings.EMAIL_HOST_USER
-            recipient_list = ['shubham.170410116032@gmail.com', ]
+            recipient_list = [user, ]
         
             # print(r[0][0])
             # print(r[0][1])
             message = client.messages.create(
                         body="Your case number: "+str(r[0][0])+", and case title: "+str(r[0][1])+", is scheduled on: "+str(i),
-                        from_='+14752566797',
-                        to='+917228801122'
+                        from_='+14xxxxxxxxx',
+                        to='+91xxxxxxxxxx'
                     )
             send_mail(subject, message, email_from, recipient_list)
             print(message.sid)
@@ -80,16 +59,16 @@ def job():
             cur.execute("SELECT case_no, title FROM backend_hearing WHERE nextHearing='"+str(i)+"' and conclusion='"+str(j)+"'")
             r = cur.fetchall()
                         
-            account_sid = 'ACee6ec8d2c1aaa94e940daa9507a6a034'
-            auth_token = '5a96e76da74c17740ac78984f98aa144'
+            account_sid = 'ACee6ec8d2c1aaa9xxxxxxxxxxxxxxxxx'
+            auth_token = '5a96e76da74c177xxxxxxxxxxxxxxxxx'
             client = Client(account_sid, auth_token)
             # print(r[0][0])
             # print(r[0][1])
             message = client.messages \
                 .create(
                         body="Your case number: "+str(r[0][0])+", and case title: "+str(r[0][1])+", is scheduled on: "+str(i),
-                        from_='+14752566797',
-                        to='+917228801122'
+                        from_='+14xxxxxxxxxx',
+                        to='+91xxxxxxxxxx'
                     )
 
             print(message.sid)
@@ -100,8 +79,8 @@ def job():
             cur.execute("SELECT case_no, title FROM backend_hearing WHERE nextHearing='"+str(i)+"' and conclusion='"+str(j)+"'")
             r = cur.fetchall()
             
-            account_sid = 'ACee6ec8d2c1aaa94e940daa9507a6a034'
-            auth_token = '5a96e76da74c17740ac78984f98aa144'
+            account_sid = '5a96e76da74c177xxxxxxxxxxxxxxxxx'
+            auth_token = '5a96e76da74c177xxxxxxxxxxxxxxxxx'
             client = Client(account_sid, auth_token)
             # print(r[0][0])
             # print(r[0][1])
